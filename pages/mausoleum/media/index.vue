2c137ecd1461e08306fdac1856559f7d6ef052d3
Amir Hosein <ah.gharache@ertebat.io>
2024-07-30 13:27:34 +0330

    fixed some stuff and pages

---
+++ pages/mausoleum/media/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <MediaPage />
+
+</template>
+<script setup lang="ts">  
+import MediaPage from "~/components/mausoleum/media/MediaPage.vue"
+defineComponent({MediaPage})
+definePageMeta({
+    layout:'empty-layout'
+})
+</script>
