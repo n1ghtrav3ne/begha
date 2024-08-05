3144e26c53890f813d1f251f0a45e4c486ce8733
Tohid Salimi <m.eshagh@ertebatbnyan.com>
2024-08-05 16:55:18 +0330

    search in media page

--- components/main-media/searchAndFilters.vue
+++ components/main-media/searchAndFilters.vue
@@ -1,113 +1,122 @@
 <template>
   <div>
     <!-- search box and search value  -->
-    <div v-if="!searchStatus">
-      <div
-        class="w-full h-14 border bg-white border-neutral-200 rounded-xl flex items-center px-3"
-      >
-        <div class="flex-none">
-          <div
-            class="flex items-center justify-center border-l border-neutral-200 pl-2 ml-2 cursor-pointer"
-            @click="filterDialog = true"
-          >
-            <span>انتخاب بقعه</span>
-            <span class="icon-Arrow-Bottom-Iran mr-1 text-2xl"> </span>
-          </div>
-        </div>
+    <div
+      class="w-full h-14 border bg-white border-neutral-200 rounded-xl flex items-center px-3"
+    >
+      <div class="flex-none">
         <div
-          class="grow cursor-pointer h-full flex items-center"
-          @click="searchStatus = true"
+          class="flex items-center justify-center border-l border-neutral-200 pl-2 ml-2 cursor-pointer"
+          @click="filterDialog = true"
         >
-          <span class="text-neutral-500">{{ search || "نام رسانه" }}</span>
-        </div>
-        <div class="flex-none">
-          <img src="~/assets/images/icons/search-black.svg" alt="" />
+          <span>انتخاب بقعه</span>
+          <span class="icon-Arrow-Bottom-Iran mr-1 text-2xl"> </span>
         </div>
       </div>
-
-      <div class="my-4" v-if="search">
-        <list
-          :title="`جستجو برای ((${searchObject.title}))`"
-          :subtitle="searchObject.number"
-          subtitle-class="text-neutral-500 text-tiny"
-          title-class="font-bold"
-        >
-          <template #prepend>
-            <div class="size-10 bg-secondary-200 rounded-full centered">
-              <div
-                class="size-5 bg-secondary-700 rounded-md text-white text-center"
-              >
-                <span class="text-[16px]">#</span>
-              </div>
-            </div>
-          </template>
-        </list>
-      </div>
-      <BottomSheets
-        v-model="filterDialog"
-        title="بقعه مورد نظر خود را انتخاب کنید"
-        :search="{ placeholder: 'جستجو بقعه' }"
-        @updateSearch="(event) => (searchFilter = event)"
+      <div
+        class="grow cursor-pointer h-full flex items-center"
+        @click="searchDialog = true"
       >
-        <div class="py-4">
-          <div class="mt-2 flex items-center">
-            <CustomCheckbox
-              label="بقاع دنبال شده"
-              v-model="tombFollowers"
-            ></CustomCheckbox>
-          </div>
-          <div class="h-1 bg-secondary-200 my-3"></div>
-          <div>
-            <list
-              title="همه"
-              @click="selectedItem = []"
-              class="my-2"
-              :class="!selectedItem.length && 'bg-secondary-200 p-3 rounded-lg'"
-            >
-              <template #prepend>
-                <img src="/assets/images/icons/menu.svg" />
-              </template>
-              <template v-if="!selectedItem.length" #append>
-                <div class="bg-secondary-700 size-6 centered rounded-full p-1">
-                  <img src="/icons/check.svg" class="w-full h-full" />
-                </div>
-              </template>
-            </list>
-            <list
-              v-for="(item, i) in listItems"
-              :key="i"
-              :title="item.title"
-              @click="selectedItemHandller(item)"
-              class="my-2 cursor-pointer"
-              :class="
-                checkItemHandller(item) && 'bg-secondary-200 p-3 rounded-lg'
-              "
-            >
-              <template #prepend>
-                <div class="centered bg-secondary-200 size-7 rounded-full">
-                  <img src="/assets/images/icons/Vector.svg" />
-                </div>
-              </template>
-              <template v-if="checkItemHandller(item)" #append>
-                <div class="bg-secondary-700 size-6 centered rounded-full p-1">
-                  <img src="/icons/check.svg" class="w-full h-full" />
-                </div>
-              </template>
-            </list>
-          </div>
-        </div>
-      </BottomSheets>
+        <span class="text-neutral-500">{{ search || "نام رسانه" }}</span>
+      </div>
+      <div class="flex-none">
+        <img src="~/assets/images/icons/search-black.svg" alt="" />
+      </div>
     </div>
 
-    <!-- search page , show after click search box (media name ) -->
-    <div v-else class="bg-white w-full z-[99] h-screen absolute top-0 right-0">
-      <div class="p-4">
-        <text-field :focus="true" v-model="search">
-          <template #append>
-            <img src="~/assets/images/icons/search-black.svg" alt="" />
-          </template>
-        </text-field>
+    <div class="my-4" v-if="search">
+      <list
+        :title="`جستجو برای ((${searchObject.title}))`"
+        :subtitle="searchObject.number"
+        subtitle-class="text-neutral-500 text-tiny"
+        title-class="font-bold"
+      >
+        <template #prepend>
+          <div class="size-10 bg-secondary-200 rounded-full centered">
+            <div
+              class="size-5 bg-secondary-700 rounded-md text-white text-center"
+            >
+              <span class="text-[16px]">#</span>
+            </div>
+          </div>
+        </template>
+      </list>
+    </div>
+    <BottomSheets
+      v-model="filterDialog"
+      title="بقعه مورد نظر خود را انتخاب کنید"
+      :search="{ placeholder: 'جستجو بقعه' }"
+      @updateSearch="(event) => (searchFilter = event)"
+    >
+      <div class="py-4">
+        <div class="mt-2 flex items-center">
+          <CustomCheckbox
+            label="بقاع دنبال شده"
+            v-model="tombFollowers"
+          ></CustomCheckbox>
+        </div>
+        <div class="h-1 bg-secondary-200 my-3"></div>
+        <div>
+          <list
+            title="همه"
+            @click="selectedItem = []"
+            class="my-2"
+            :class="!selectedItem.length && 'bg-secondary-200 p-3 rounded-lg'"
+          >
+            <template #prepend>
+              <img src="/assets/images/icons/menu.svg" />
+            </template>
+            <template v-if="!selectedItem.length" #append>
+              <div class="bg-secondary-700 size-6 centered rounded-full p-1">
+                <img src="/icons/check.svg" class="w-full h-full" />
+              </div>
+            </template>
+          </list>
+          <list
+            v-for="(item, i) in listItems"
+            :key="i"
+            :title="item.title"
+            @click="selectedItemHandller(item)"
+            class="my-2 cursor-pointer"
+            :class="
+              checkItemHandller(item) && 'bg-secondary-200 p-3 rounded-lg'
+            "
+          >
+            <template #prepend>
+              <div class="centered bg-secondary-200 size-7 rounded-full">
+                <img src="/assets/images/icons/Vector.svg" />
+              </div>
+            </template>
+            <template v-if="checkItemHandller(item)" #append>
+              <div class="bg-secondary-700 size-6 centered rounded-full p-1">
+                <img src="/icons/check.svg" class="w-full h-full" />
+              </div>
+            </template>
+          </list>
+        </div>
+      </div>
+    </BottomSheets>
 
+    <!-- search page , show after click search box (media name ) -->
+    <BottomSheets v-model="searchDialog" :fullscreen="true" empty>
+      <div class="p-4">
+        <div class="flex items-center">
+          <span
+            @click="searchDialog = false"
+            class="material-symbols-outlined back-icon ml-2"
+          >
+            arrow_right_alt
+          </span>
+          <div class="w-full">
+            <text-field placeholder="جستجوی بقعه">
+              <template #append>
+                <div class="cursor-pointer ml-2">
+                  <img src="~/assets/images/icons/search-black.svg" alt="" />
+                </div>
+              </template>
+            </text-field>
+          </div>
+        </div>
         <div class="mt-2">
           <template
             v-for="(item, i) in mediase.filter((x) => x.title.includes(search))"
@@ -120,7 +129,7 @@
               class="py-2 my-2 cursor-pointer"
               subtitle-class="text-tiny text-neutral-500"
               @click="
-                searchStatus = false;
+                searchDialog = false;
                 search = item.title;
                 searchObject = item;
               "
@@ -152,7 +161,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </BottomSheets>
   </div>
 </template>
 
@@ -160,7 +169,7 @@
 const filterDialog = ref(false);
 const searchFilter = ref("");
 const tombFollowers = ref(false);
-const searchStatus = ref(null);
+const searchDialog = ref(null);
 const selectedItem = ref([]);
 
 const search = ref("");
