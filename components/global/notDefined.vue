4b287a379f0e3d877c68223f5b4519b1ed138f5c
Amir Hosein <ah.gharache@ertebat.io>
2024-08-06 17:04:02 +0330

    begha media got fixed

--- components/global/notDefined.vue
+++ components/global/notDefined.vue
@@ -2,13 +2,13 @@
   <div class="h-96 w-full centered">
     <div class="centered flex-col">
       <img class="notfound-img" src="@/assets/images/no-item.svg" />
-      <span class="mt-3">{{ title }}ی تعریف نشده است.</span>
+      <span class="mt-3">{{ title }}</span>
       <NuxtLink :to="link">
-        <button
+        <button v-if="active"
           class="bg-primary-700 text-white mt-3 flex justify-center items-center gap-2 py-2 px-4 w-full h-11 rounded-lg"
         >
           <img src="@/assets/images/icons/add-square.svg" />
-          {{ btnTitle || title }} جدید
+          {{ btnTitle || title }}
         </button>
       </NuxtLink>
     </div>
@@ -26,6 +26,10 @@ const props = defineProps({
   link: {
     type: String,
   },
+  active: {
+    type: Boolean,
+    default:true
+  }
 });
 </script>
 
